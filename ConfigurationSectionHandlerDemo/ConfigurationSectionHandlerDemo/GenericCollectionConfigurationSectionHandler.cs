e7bbac86ed3f203de375442b002b6ad5a94b5be7
Son Nguyen <dinh.son.nguyen@gmail.com>
2015-10-12 13:51:41 +0700

    Demo for handling configuration section with a collection of configs.

---
+++ ConfigurationSectionHandlerDemo/ConfigurationSectionHandlerDemo/GenericCollectionConfigurationSectionHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace ConfigurationSectionHandlerDemo
+{
+    public sealed class GenericCollectionConfigurationSectionHandler<T> : IConfigurationSectionHandler
+    {
+        private string _rootElementName;
+
+        private string RootElementName
+        {
+            get
+            {
+                if (_rootElementName != null)
+                {
+                    return _rootElementName;
+                }
+
+                var xmlRootAttributes = typeof (T).GetCustomAttributes(typeof (XmlRootAttribute), false)
+                                                  .Cast<XmlRootAttribute>()
+                                                  .ToList();
+
+                return _rootElementName = xmlRootAttributes.Any() ? xmlRootAttributes.First().ElementName : typeof (T).Name;
+            }
+        }
+
+        public object Create(object parent, object configContext, XmlNode section)
+        {
+            var xmlSerializer = new XmlSerializer(typeof (T));
+
+            var xmlNodeReader = new XmlNodeReader(section);
+
+            return XDocument.Load(xmlNodeReader)
+                            .Descendants(RootElementName)
+                            .Select(e => (T) xmlSerializer.Deserialize(e.CreateReader()))
+                            .ToArray();
+        }
+    }
+}
---
+++ ConfigurationSectionHandlerDemo/ConfigurationSectionHandlerDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.Xml.Serialization;
+
+namespace ConfigurationSectionHandlerDemo
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            TestGenericCollectionConfigurationSectionHandler();
+
+            Console.ReadLine();
+        }
+
+        private static void TestGenericCollectionConfigurationSectionHandler()
+        {
+            var pageSections = (Page[]) ConfigurationManager.GetSection("page");
+
+            Console.WriteLine("Found {0} pages\n", pageSections.Length);
+
+            foreach (var indexing in pageSections)
+            {
+                Console.WriteLine("Page name: {0}", indexing.Name);
+                Console.WriteLine("Searchable: {0}\n", indexing.Property.Searchable);
+            }
+        }
+    }
+
+    [XmlRoot("article")]
+    public class Page
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlElement("property")]
+        public PageTypeProperty Property { get; set; }
+    }
+
+    public class PageTypeProperty
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("searchable")]
+        public bool Searchable { get; set; }
+    }
+}
