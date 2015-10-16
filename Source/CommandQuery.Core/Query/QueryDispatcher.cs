5459ddb843b9b4db927aa04f9fba07b427288ae7
Son Nguyen <dinh.son.nguyen@gmail.com>
2015-10-16 20:42:23 +0700

    Add QueryDispatcher implementation.

---
+++ Source/CommandQuery.Core/Query/QueryDispatcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CommandQuery.Core.Query
+{
+    public class QueryDispatcher : IQueryDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public QueryDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public TResult Dispatch<TResult>(Query<TResult> query)
+        {
+            var handlerType = typeof (IQueryHandler<,>).MakeGenericType(query.GetType(), typeof (TResult));
+
+            var handler = (IQueryHandler) _serviceProvider.GetService(handlerType);
+
+            return (TResult) handler.Handle(query);
+        }
+    }
+}
