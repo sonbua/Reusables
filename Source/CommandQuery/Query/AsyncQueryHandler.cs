7812cadec165ceff88748fa572ce0e606788cf4f
Son Nguyen <dinh.son.nguyen@gmail.com>
2015-10-18 22:15:58 +0700

    Add async query handler.

---
+++ Source/CommandQuery/Query/AsyncQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace CommandQuery.Query
+{
+    public abstract class AsyncQueryHandler<TQuery, TResult> : IQueryHandler<TQuery, Task<TResult>> where TQuery : Query<Task<TResult>>
+    {
+        public abstract Task<TResult> Handle(TQuery query);
+
+        public object Handle(object query)
+        {
+            return Handle((TQuery) query);
+        }
+    }
+}
