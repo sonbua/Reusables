30418ef5057196a3e7701cc94536f184fdc4cf52
Son Nguyen <dinh.son.nguyen@gmail.com>
2015-10-26 20:29:53 +0700

    Add validator for StringLengthAttribute.

--- Source/Reusables.DataAnnotations/StringLengthAttribute.cs
+++ Source/Reusables.DataAnnotations/StringLengthAttribute.cs
@@ -1,14 +1,41 @@
-﻿namespace Reusables.DataAnnotations
+﻿using System;
+
+namespace Reusables.DataAnnotations
 {
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public class StringLengthAttribute : ValidationAttribute
     {
+        private int _minimumLength;
+
         public StringLengthAttribute(int maximumLength)
         {
+            if (maximumLength < 0)
+            {
+                throw new ArgumentException("Maximum length should be greater than or equal to 0.");
+            }
+
             MaximumLength = maximumLength;
         }
 
         public int MaximumLength { get; private set; }
 
-        public int MinimumLength { get; set; }
+        public int MinimumLength
+        {
+            get { return _minimumLength; }
+            set
+            {
+                if (value > MaximumLength)
+                {
+                    throw new ArgumentException("Minimum length should be less than or equal to maximum length.");
+                }
+
+                if (value < 0)
+                {
+                    throw new ArgumentException("Minimum length should be greater than or equal to 0.");
+                }
+
+                _minimumLength = value;
+            }
+        }
     }
 }
---
+++ Source/Reusables.Validation/DataAnnotations/StringLengthAttributeValidator.cs
@@ -0,0 +1,31 @@
+﻿using Reusables.DataAnnotations;
+
+namespace Reusables.Validation.DataAnnotations
+{
+    public class StringLengthAttributeValidator : ValidationAttributeValidator<StringLengthAttribute>
+    {
+        public override ValidationResult Validate(object value, ValidationContext context, StringLengthAttribute attribute)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+
+            var num = ((string) value).Length;
+
+            if (num >= attribute.MinimumLength && num <= attribute.MaximumLength)
+            {
+                return ValidationResult.Success;
+            }
+            
+            if (num < attribute.MinimumLength)
+            {
+                // TODO: resource
+                return new ValidationResult(string.Format("{0} should contain at least {1} characters.", context.MemberName, attribute.MinimumLength), context.MemberName);
+            }
+
+            // TODO: resource
+            return new ValidationResult(string.Format("{0} should contain at most {1} characters.", context.MemberName, attribute.MaximumLength), context.MemberName);
+        }
+    }
+}
