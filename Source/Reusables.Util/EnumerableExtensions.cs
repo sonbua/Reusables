244aab8500779d419706ed39d2f8e263939aa100
Son Nguyen <dinh.son.nguyen@gmail.com>
2015-10-31 01:06:28 +0700

    Add more IEnumerable's extension methods: Chunk, IsEmpty, None. Add object's extension method: ToDictionary.

--- Source/Reusables.Util/EnumerableExtensions.cs
+++ Source/Reusables.Util/EnumerableExtensions.cs
@@ -6,54 +6,10 @@ using System.Text;
 namespace Reusables.Util.Extensions
 {
     /// <summary>
-    /// All extension methods related to IEnumarable&lt;T&gt;.
+    /// All extension methods related to <see cref="IEnumerable{T}"/>.
     /// </summary>
     public static class EnumerableExtensions
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="IEnumerable&lt;T&gt;"/> to check for nullness or emptiness.</param>
-        /// <returns><c>true</c> if the source sequence contains any elements; otherwise, false.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
-        {
-            if (source == null)
-            {
-                return true;
-            }
-
-            using (var enumerator = source.GetEnumerator())
-            {
-                return !enumerator.MoveNext();
-            }
-        }
-
-        /// <summary>
-        /// Repeats an action for each element in an array or an object collection that implements the System.Collections.IEnumerable or System.Collections.Generic.IEnumerable&lt;T&gt; interface.
-        /// </summary>
-        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">A sequence of values to invoke a function on.</param>
-        /// <param name="action">A function to apply to each object of the sequence.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="action"/> is null.</exception>
-        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
-        {
-            if (source == null)
-            {
-                throw new ArgumentNullException("source");
-            }
-            if (action == null)
-            {
-                throw new ArgumentNullException("action");
-            }
-
-            foreach (var item in source)
-            {
-                action(item);
-            }
-        }
-
         /// <summary>
         /// Build a string from a sequence of objects.
         /// </summary>
@@ -86,6 +42,99 @@ namespace Reusables.Util.Extensions
             return stringBuilder.ToString();
         }
 
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunkSize)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (chunkSize <= 0)
+            {
+                throw new ArgumentException("Chunk size must be at least 1.");
+            }
+
+            var sourceArray = source.ToArray();
+
+            for (var i = 0; i < sourceArray.Length; i += chunkSize)
+            {
+                yield return sourceArray.Skip(i).Take(chunkSize);
+            }
+        }
+
+        /// <summary>
+        /// Repeats an action for each element in an array or an object collection that implements the System.Collections.IEnumerable or System.Collections.Generic.IEnumerable&lt;T&gt; interface.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">A sequence of values to invoke a function on.</param>
+        /// <param name="action">A function to apply to each object of the sequence.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="action"/> is null.</exception>
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+
+        /// <summary>
+        /// Determines whether a sequence is empty.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> whose elements to apply the predicate to.</param>
+        /// <returns></returns>
+        public static bool IsEmpty<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return !source.Any();
+        }
+
+        /// <summary>
+        /// Check whether a collection contains an specified item.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="collection"/>.</typeparam>
+        /// <param name="item">The item to check.</param>
+        /// <param name="collection">A sequence in which to locate a value.</param>
+        /// <returns><c>true</c> if item is in the collection, otherwise <c>false</c>. Always returns <c>false</c> if item is null.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="collection"/> is null.</exception>
+        public static bool IsIn<T>(this T item, IEnumerable<T> collection)
+        {
+            return collection.Contains(item);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">The <see cref="IEnumerable&lt;T&gt;"/> to check for nullness or emptiness.</param>
+        /// <returns><c>true</c> if the source sequence contains any elements; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                return true;
+            }
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                return !enumerator.MoveNext();
+            }
+        }
+
         /// <summary>
         /// Concatenates the members of a constructed collection of arbitrary objects, using the specified separator between each member.
         /// </summary>
@@ -101,16 +150,25 @@ namespace Reusables.Util.Extensions
         }
 
         /// <summary>
-        /// Check whether a collection contains an specified item.
+        /// Determines whether no element of a sequence satisfies a condition.
         /// </summary>
-        /// <typeparam name="T">The type of the elements of <paramref name="collection"/>.</typeparam>
-        /// <param name="item">The item to check.</param>
-        /// <param name="collection">A sequence in which to locate a value.</param>
-        /// <returns><c>true</c> if item is in the collection, otherwise <c>false</c>. Always returns <c>false</c> if item is null.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="collection"/> is null.</exception>
-        public static bool IsIn<T>(this T item, IEnumerable<T> collection)
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> whose elements to apply the predicate to.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns><c>true</c> if there's no element in the source sequence that satisfies the condition; otherwise, <c>false</c>.</returns>
+        public static bool None<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
-            return collection.Contains(item);
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException("predicate");
+            }
+
+            return !source.Any(predicate);
         }
     }
 }
---
+++ Source/Reusables.Util/ObjectExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Reusables.Util.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Extract all public properties' names and associated values from an object instance to a dictionary. The instance must have an empty constructor.
+        /// </summary>
+        /// <param name="source">The object to retrieve property values.</param>
+        /// <returns></returns>
+        public static Dictionary<string, object> ToDictionary(this object source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return source.GetType()
+                         .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                         .ToDictionary(x => x.Name, x => x.GetValue(source));
+        }
+    }
+}
