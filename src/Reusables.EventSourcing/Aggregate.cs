2970d68e2433009502669f291912e8397adc318a
Son Nguyen <dinh.son.nguyen@gmail.com>
2015-11-09 22:27:33 +0700

    Add asynchronous repository interface.

---
+++ src/Reusables.EventSourcing/Aggregate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Reusables.EventSourcing
+{
+    public abstract class Aggregate
+    {
+        public abstract Guid Id { get; }
+
+        public long Version { get; set; }
+    }
+}
---
+++ src/Reusables.EventSourcing/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Reusables.EventSourcing
+{
+    public interface IRepository
+    {
+        Task<TAggregate> GetById<TAggregate>(Guid id) where TAggregate : Aggregate;
+
+        Task Save(Aggregate aggregate);
+    }
+}
