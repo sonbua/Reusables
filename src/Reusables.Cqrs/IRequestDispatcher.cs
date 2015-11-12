2ad03dd68addb48d9602e494bc910b779a420598
Son Nguyen <dinh.son.nguyen@gmail.com>
2015-11-12 13:03:18 +0700

    Add a request dispatcher that composes of a command dispatcher and a query dispatcher.

---
+++ src/Reusables.Cqrs/IRequestDispatcher.cs
@@ -0,0 +1,9 @@
+﻿namespace Reusables.Cqrs
+{
+    /// <summary>
+    /// Defines an interface for a request dispatcher (of both command type and query type). This dispatcher is responsible for distributing a command/query object to the right command/query handler, letting it process the command/query, then returning result back to the caller (if any).
+    /// </summary>
+    public interface IRequestDispatcher : ICommandDispatcher, IQueryDispatcher
+    {
+    }
+}
---
+++ src/Reusables.Cqrs/RequestDispatcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Reusables.Cqrs
+{
+    /// <summary>
+    /// Provides a default implementation of the <see cref="IRequestDispatcher"/> interface.
+    /// </summary>
+    public class RequestDispatcher : IRequestDispatcher, ICommandDispatcher, IQueryDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public RequestDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        void ICommandDispatcher.Dispatch<TCommand>(TCommand command)
+        {
+            var handler = (ICommandHandler<TCommand>) _serviceProvider.GetService(typeof (ICommandHandler<TCommand>));
+
+            handler.Handle(command);
+        }
+
+        async Task ICommandDispatcher.DispatchAsync<TAsyncCommand>(TAsyncCommand command)
+        {
+            var handler = (IAsyncCommandHandler<TAsyncCommand>) _serviceProvider.GetService(typeof (IAsyncCommandHandler<TAsyncCommand>));
+
+            await handler.HandleAsync(command).ConfigureAwait(false);
+        }
+
+        TResult IQueryDispatcher.Dispatch<TResult>(Query<TResult> query)
+        {
+            var handlerType = typeof (IQueryHandler<,>).MakeGenericType(query.GetType(), typeof (TResult));
+
+            var handler = (IQueryHandler<TResult>) _serviceProvider.GetService(handlerType);
+
+            return handler.Handle(query);
+        }
+
+        async Task<TResult> IQueryDispatcher.DispatchAsync<TResult>(AsyncQuery<TResult> query)
+        {
+            var handlerType = typeof (IAsyncQueryHandler<,>).MakeGenericType(query.GetType(), typeof (TResult));
+
+            var handler = (IAsyncQueryHandler<TResult>) _serviceProvider.GetService(handlerType);
+
+            return await handler.HandleAsync(query).ConfigureAwait(false);
+        }
+    }
+}
