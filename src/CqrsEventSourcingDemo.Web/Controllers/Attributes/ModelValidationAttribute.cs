12757a8bbd4404cf6fc0418a7e2be881d3732dc1
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-01-19 16:29:23 +0700

    Enable to validate model after model binding occurred and before it gets passed to action.

---
+++ src/CqrsEventSourcingDemo.Web/Controllers/Attributes/ModelValidationAttribute.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Reusables.Validation;
+using Reusables.Web.Mvc5;
+using FilterAttribute = Reusables.Web.Mvc5.FilterAttribute;
+
+namespace CqrsEventSourcingDemo.Web.Controllers.Attributes
+{
+    public class ModelValidationAttribute : FilterAttribute
+    {
+    }
+
+    public class ValidateModel : IActionFilter<ModelValidationAttribute>
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ValidateModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public int Order => 0;
+
+        public bool SkipNextFilters => false;
+
+        public void OnActionExecuting(ModelValidationAttribute attribute, ActionExecutingContext filterContext)
+        {
+            var actionParameters = filterContext.ActionParameters;
+
+            if (actionParameters.Count == 0)
+            {
+                return;
+            }
+
+            var model = actionParameters.Select(x => x.Value).FirstOrDefault(x => x.GetType().IsClass);
+
+            if (model == null)
+            {
+                return;
+            }
+
+            var modelValidatorType = typeof (IValidator<>).MakeGenericType(model.GetType());
+
+            dynamic validator = _serviceProvider.GetService(modelValidatorType);
+
+            IEnumerable<ValidationResult> validationResults = validator.Validate((dynamic) model);
+
+            foreach (var result in validationResults)
+            {
+                filterContext.Controller.ViewData.ModelState.AddModelError(result.MemberName, result.ErrorMessage);
+            }
+        }
+
+        public void OnActionExecuted(ModelValidationAttribute attribute, ActionExecutedContext filterContext)
+        {
+            // do nothing
+        }
+    }
+}
