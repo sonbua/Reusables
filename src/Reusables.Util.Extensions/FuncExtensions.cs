d5d30c46383085bf25634c17f30a59547d862ebf
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-01-25 21:53:33 +0700

    Enable to chain Func with FolllowedBy extension method.

---
+++ src/Reusables.Util.Extensions/FuncExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reusables.Util.Extensions
+{
+    public static class FuncExtensions
+    {
+        public static Func<TIn, TResult> FollowedBy<TIn, TTemp, TResult>(this Func<TIn, TTemp> firstFunc, Func<TTemp, TResult> nextFunc)
+        {
+            return input => nextFunc(firstFunc(input));
+        }
+    }
+}
