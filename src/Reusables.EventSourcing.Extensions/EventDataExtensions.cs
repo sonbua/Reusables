0410e30d1580dcd7bf1e184b47a1adf46762d9d5
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-12-05 04:33:03 +0700

    Refactor.

--- src/Reusables.EventSourcing.Extensions/EventDataExtensions.cs
+++ src/Reusables.EventSourcing.Extensions/EventDataExtensions.cs
@@ -7,6 +7,8 @@ namespace Reusables.EventSourcing.Extensions
 {
     public static class EventDataExtensions
     {
+        private const string _EVENT_CLR_TYPE = "EventClrType";
+
         private static readonly JsonSerializerSettings _serializerSettings = new JsonSerializerSettings {TypeNameHandling = TypeNameHandling.None};
 
         public static EventData ToEventData<TEvent>(this TEvent @event, string aggregateType, Guid aggregateId, long version)
@@ -14,28 +16,28 @@ namespace Reusables.EventSourcing.Extensions
             var eventId = Guid.NewGuid();
             var data = JsonConvert.SerializeObject(@event, _serializerSettings);
             var eventHeaders = new Dictionary<string, object>
-                               {
-                                   {
-                                       "EventClrType", @event.GetType().AssemblyQualifiedName
-                                   }
-                               };
+            {
+                {
+                    _EVENT_CLR_TYPE, @event.GetType().AssemblyQualifiedName
+                }
+            };
             var metadata = JsonConvert.SerializeObject(eventHeaders, _serializerSettings);
 
             return new EventData
-                   {
-                       Id = eventId,
-                       Created = DateTime.UtcNow,
-                       AggregateType = aggregateType,
-                       AggregateId = aggregateId,
-                       Version = version,
-                       Event = data,
-                       Metadata = metadata,
-                   };
+            {
+                Id = eventId,
+                Created = DateTime.UtcNow,
+                AggregateType = aggregateType,
+                AggregateId = aggregateId,
+                Version = version,
+                Event = data,
+                Metadata = metadata,
+            };
         }
 
         public static object FromEventData(this EventData eventData)
         {
-            var eventClrTypeName = JObject.Parse(eventData.Metadata).Property("EventClrType").Value;
+            var eventClrTypeName = JObject.Parse(eventData.Metadata).Property(_EVENT_CLR_TYPE).Value;
 
             return JsonConvert.DeserializeObject(eventData.Event, Type.GetType((string) eventClrTypeName));
         }
